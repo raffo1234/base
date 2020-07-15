c41deb38e95270ac6a9865958b0f2c56305c30c4
Rafael Meza <rhmcord@gmail.com>
2020-07-15 16:14:03 -0500

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Stack } from "@ableco/baseline";
 import Home from "components/Home";
+import Dashboard from "components/Dashboard";
 
 function App() {
-  return <Home></Home>;
+  return (
+    <Router>
+      <Stack as="nav">
+        <Stack as="ul">
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </Stack>
+        <hr />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Stack>
+    </Router>
+  );
 }
 
 export default App;
