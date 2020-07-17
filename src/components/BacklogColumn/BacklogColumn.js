f673773b045cb2436b54113bceb11e453ce6cdb9
Rafael Meza <rhmcord@gmail.com>
2020-07-17 11:24:06 -0500

    Add Backlog column

---
+++ src/components/BacklogColumn/BacklogColumn.js
@@ -0,0 +1,84 @@
+import React from "react";
+import { Stack, Button, Flex, Text } from "@ableco/baseline";
+import { ReactComponent as CloseIcon } from "icons/close.svg";
+import { Lock } from "@baseline/icons";
+
+const BacklogColumn = () => {
+  return (
+    <>
+      <Flex className="justify-between mb-6 w-full">
+        <Text as="h1">Backlog</Text>
+        <Button
+          variant="solid"
+          color="primary"
+          className="rounded-lg py-2 px-4"
+        >
+          <Text color="white" size="s">
+            + Add
+          </Text>
+        </Button>
+      </Flex>
+      <Stack className="overflow-auto flex-grow w-full">
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          <Flex>
+            <Lock className="w-6 h-6 fill-current text-neutral-400" />
+            <CloseIcon className="w-6 h-6 fill-current text-neutral-400" />
+          </Flex>
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+        <Stack className="w-full bg-white px-4 py-2 rounded-md mb-2">
+          OKRs Hierarchy
+        </Stack>
+      </Stack>
+    </>
+  );
+};
+export default BacklogColumn;
